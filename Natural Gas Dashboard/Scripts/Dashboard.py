2cb9e00a85e649bd9161f45d019d5595ef55293a
MatthewTe <teelucksingh.matthew1@gmail.com>
2019-08-06 22:19:25 -0400

    Wrote the database_reutrn moduel that returns natural gas prices and critically also wrote a rough draft for the main script Dashboard- plotting the Natural Gas Price over time

--- Natural Gas Dashboard/Scripts/Dashboard.py
+++ Natural Gas Dashboard/Scripts/Dashboard.py
@@ -1,14 +1,28 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from Database_return import gas_prices
+
 
 # Deffining the Application:
 app = dash.Dash()
 
 # Describing the app layout:
-app.layout = html.Div(childern=[
+app.layout = html.Div(children=[
 
-])
+# Creating a dash_core_components graph object as a child:
+    dcc.Graph(
+        id = "Natural Gas Prices",
+        figure = {
+            'data' : [
+            {'x': gas_prices().index, 'y': gas_prices(),'type': 'line', 'name': 'Nat Gas'}],
+            'layout' : {
+                'title': "Natural Gas Price"
+                }
+            }
+        )
+    ]
+)
 # Running the dashboard server:
 def dashboard_run(Bool):
     if Bool == True:
